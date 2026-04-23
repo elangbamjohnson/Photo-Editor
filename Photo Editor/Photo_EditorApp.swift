693e1da2ec7f2f4ea0df5632d07b39aabbe8176c
Johnson <elangbamj@avaya.com>
2026-04-23 18:58:24 +0530

    Initial commit

---
+++ Photo Editor/Photo_EditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_EditorApp.swift
+//  Photo Editor
+//
+//  Created by Johnson on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Photo_EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
